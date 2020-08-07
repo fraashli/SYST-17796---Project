daa8a450ae304c73603924bc98e6199c18f7f74f
Sonaldeep <nijjers@sheridancollege.ca>
2020-08-07 15:08:39 -0400

    adding variables

---
+++ src/ca/sheridancollege/project/Deck.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author sonaldeep
+ */
+import java.util.Random;
+public class Deck {
+   private int _count;
+   private Card[] _hand;
+   private static final int Storage = 52;
+   private static Random random = new Random();
+}
