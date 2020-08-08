9352e1a41e81e776a5ef909e12244e1587306020
noorapatel <patelno@sheridancollege.ca>
2020-08-07 23:54:10 -0400

    created method called intialize user hands for the players

---
+++ src/ca/sheridancollege/project/GoFish.java
@@ -0,0 +1,75 @@
+/**
+ * SYST 17796 Project Winter 2020 Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as a modifier and the date!
+ */
+package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/**
+ * A concrete class that represents any grouping of cards for a Game.
+ * HINT, you might want to subclass this more than once.
+ * The group of cards has a maximum size attribute which is flexible for reuse.
+ * @author Sivagama
+ * @author Noora Naz Patel
+ */
+public class GoFish 
+{
+    private Deck player;
+    private Deck declareWinner;
+    private Deck takeCard;
+    private boolean GameOver;
+    private Deck computerTurn;
+   
+   
+    //The group of cards, stored in an ArrayList
+    private ArrayList <Card> cards;
+    private int size;//the size of the grouping
+    
+    public GoFish(int givenSize)
+    {
+        size = givenSize;
+        takeCard=new Deck();
+        player=new Deck();
+        computerTurn= new Deck();
+        
+    }
+    
+    /**
+     * A method that will get the group of cards as an ArrayList
+     * @return the group of cards.
+     */
+    public ArrayList<Card> showCards()
+    {
+        return cards;
+    }
+    
+    public void shuffle()
+    {
+        Collections.shuffle(cards);
+    }
+
+    /**
+     * @return the size of the group of cards
+     */
+    public int getSize() {
+        return size;
+    }
+public void playerTurn(){
+    intializeUserhand();
+}
+    /**
+     * @param givenSize the max size for the group of cards
+     */
+    public void setSize(int givenSize) {
+        size = givenSize;
+    }
+
+    private void intializeUserhand() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}//end class
